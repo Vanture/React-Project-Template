790e247ecfe07ac22e57162c9f619f63c13b33c6
hugmanrique <hugmanrique@gmail.com>
2017-06-25 14:53:44 -0400

    Redo entire webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,104 +5,110 @@ const path = require('path');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const production = process.env.NODE_ENV === 'production';
-const watchMode = process.argv.indexOf('--watch') !== -1;
 
 const BUILD_DIR = path.resolve(__dirname, 'build');
 const APP_DIR = path.resolve(__dirname, 'src');
 
-const webpackConfig = {
-  context: __dirname,
-  entry: {
-    app: APP_DIR + '/index.js'
-  },
-  output: {
-    path: BUILD_DIR,
-    filename: '[name].js'
-  },
-  devtool: production ? 'source-map' : 'eval-source-map',
-  stats: {
-    children: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-                modules: true,
-                getLocalIdent: (context, localIdentName, localName, options) => {
-                  if(!options.context) {
-		                options.context = context.options && typeof context.options.context === "string" ? context.options.context : context.context;
-                  }
+module.exports = ({ production, publicPath = '/'}) => {
+  const isProd = production === true;
+  const publicUrl = publicPath.slice(0, -1);
 
-                  let request = path.relative(path.join(__dirname, "src"), context.resourcePath);
-
-                  request = request.replace(/\.[^/.]+$/, "");
-                  request = request.replace(/\/index$/, "");
-                  request = request.replace(/\//g, "_");
-                  request = request.replace(/[^a-z0-9-_]/gi, "");
-
-                  // [path]_[className]
-                  // i.e. components_App_container
-                  return `${request}_${localName}`;
+  const config = {
+    context: __dirname,
+    entry: {
+      app: APP_DIR + '/index.js'
+    },
+    output: {
+      path: BUILD_DIR,
+      filename: '[name].js',
+      publicPath
+    },
+    devtool: isProd ? 'cheap-module-source-map' : 'source-map',
+    stats: {
+      children: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[hash:base64:5]_local'
+                }
+              },
+              {
+                loader: 'fast-sass-loader',
+                options: {
+                  includePaths: [
+                    path.resolve(APP_DIR, 'sass')
+                  ]
                 }
               }
-            },
+            ]
+          })
+        },
+        {
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: [
             {
-              loader: 'fast-sass-loader',
+              loader: 'file-loader',
               options: {
-                includePaths: [
-                  path.resolve(__dirname, 'src/sass')
-                ],
-                data: '@import "_variables";'
+                name: '[sha512:hash:base64:7]-[name].[ext]'
               }
             }
           ]
-        })
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[sha512:hash:base64:7]-[name].[ext]'
-            }
-          }
-        ]
-      }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+        'process.env.PUBLIC_URL' : JSON.stringify(publicUrl)
+      }),
+      new HtmlWebpackPlugin({
+        title: 'App Name',
+        template: path.resolve(APP_DIR, 'template.ejs'),
+        publicUrl
+      }),
+      new ExtractTextPlugin({
+        filename: 'styles.css',
+        allChunks: true
+      })
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    }),
-    new HtmlWebpackPlugin({
-      title: 'My App',
-      template: 'src/template.ejs'
-    }),
-    new ExtractTextPlugin({
-      filename: 'main.css',
-      allChunks: true
-    })
-  ]
+  };
+
+  if (production) {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: true,
+        compress: {
+          warnings: false,
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          screw_ie8: true
+        },
+        output: {
+          comments: false
+        },
+        exclude: [
+          /\.min\.js$/gi // Skip pre-minified libs
+        ]
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+    );
+  }
+
+  return config;
 };
-
-if (!watchMode && production) {
-  webpackConfig.plugins.push(new UglifyJSPlugin());
-}
-
-module.exports = webpackConfig;
